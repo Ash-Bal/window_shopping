54ee1fa82f9c237b7975b5340596e7033a4062fc
Ashvath Balgovind <ashvath@ashvaths-mbp.partners.org>
2024-11-15 15:14:28 -0500

    finalize image_captioning

--- marketplace_assistant/image_captioning.py
+++ marketplace_assistant/image_captioning.py
@@ -8,38 +8,33 @@ import requests
 from PIL import Image
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from marketplace_assistant.config import (
-    RAW_IMAGE_FILE_DIR,
-    PROCESSED_DATA_DIR,
-    GOOG_RAW_IMAGE_FILE_DIR,
-    GOOG_PROCESSED_DATA_DIR,
-)
+from marketplace_assistant.config import RAW_IMAGE_FILE_DIR, PROCESSED_DATA_DIR
 
 
 def main():
     model_id = "vikhyatk/moondream2"
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-        model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True).to(device)
-    elif torch.backends.mps.is_available():
-        model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True).to("mps")
-    else:
-        model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+    print(f"Device: {device}")
+    model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True).to(device)
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-    marketplace_data_path = GOOG_PROCESSED_DATA_DIR / "marketplace_data.csv"
+    marketplace_data_path = PROCESSED_DATA_DIR / "marketplace_data.csv"
     marketplace_data = pd.read_csv(
         marketplace_data_path,
     )
 
-    batch_size = 2
-    batch_sample = marketplace_data.sample(4)
+    batch_size = 25
+    batch_sample = marketplace_data
     data_len = len(batch_sample)
 
     ans = []
 
     for chunk in tqdm(np.array_split(batch_sample, data_len / batch_size)):
-        chunk["full_path"] = str(GOOG_RAW_IMAGE_FILE_DIR) + "/" + chunk["path"].astype(str)
+        chunk["full_path"] = str(RAW_IMAGE_FILE_DIR) + "/" + chunk["path"].astype(str)
         chunk_imgs = chunk["full_path"].tolist()
         chunk_imgs = [Image.open(name) for name in chunk_imgs]
         prompt = [
@@ -49,9 +44,7 @@ def main():
 
     batch_sample["ai_desc"] = ans
 
-    batch_sample.to_csv(
-        GOOG_PROCESSED_DATA_DIR / "marketplace_data_captioned_images.csv", index=False
-    )
+    batch_sample.to_csv(PROCESSED_DATA_DIR / "marketplace_data_captioned_images.csv", index=False)
 
 
 if __name__ == "__main__":
